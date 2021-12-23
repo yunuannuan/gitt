e3e73f875a03ede33fca416759685d3cee136158
shi <2459857209@qq.com>
2021-12-23 17:00:53 +0800

    实验1

---
+++ src/main/java/com/GittApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GittApplication.class, args);
+    }
+
+}
